71558cd80f9cec9efa613538484e462619aecbc0
EnzoCam7 <camerac09@gmail.com>
2023-11-06 20:32:03 -0300

    Feat: add fetch to every ID

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,22 @@
 import type { Metadata } from "next";
 import "./globals.css";
 
-export default function RootLayout({
+async function ListItem({ id }: { id: number }) {
+  const post = await fetch(
+    `https://hacker-news.firebaseio.com/v0/item/${id}.json?print=pretty`
+  ).then(res => res.json() as Promise<{text: string; title: string; by: string}>);
+  return <li>{post.text || post.title}</li>;
+}
+
+export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const posts = await fetch(
+    `https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty&limitToFirst=10&orderBy="$key"`
+  ).then((res) => res.json() as Promise<number[]>);
+  console.log(posts);
   return (
     <html lang="en">
       <body className="grid grid-rows-[60px_1fr] h-screen">
@@ -13,7 +24,13 @@ export default function RootLayout({
           UIZard Hackernews Reader
         </header>
         <main className="grid grid-cols-[320px_1fr]">
-          <aside>Leftbar</aside>
+          <aside>
+            <ul>
+              {posts.map((post) => (
+                <ListItem key={post} id={post} />
+              ))}
+            </ul>
+          </aside>
           <section>{children}</section>
         </main>
       </body>
